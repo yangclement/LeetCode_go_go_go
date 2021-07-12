f5fcd39bef86de0a0f0292f64ee84b03db485b0a
yangclement <54861086+yangclement@users.noreply.github.com>
2021-07-11 18:19:36 -0700

    Time: 28 ms (83.42%), Space: 54 MB (66.82%) - LeetHub

--- sliding-window-maximum/sliding-window-maximum.java
+++ sliding-window-maximum/sliding-window-maximum.java
@@ -12,17 +12,18 @@ class Solution {
         //这里使用linkedlist一样的，都是可以双开口，头和尾都可以remove和add
         LinkedList<Integer> queue = new LinkedList<Integer>();
         int[] results = new int[nums.length - k + 1];
-        //[3 -1] k = 3, i = 3
-        //[3 -1 -3]
-        //[5 3] 
-        //用queue来记录index好像更高效？
+        //用queue来记录index好像更高效，因为你可以根据index去找数字，但是你不能根据数字去找index
+        //还需要用index来看当前的最大值是不是已经out of sliding windows了
         int j = 0;
         for (int i = 0; i < nums.length; i++) {
-            
-            //valid 的最大值已经超出
+            //valid 的最大值已经超出window size
             if (!queue.isEmpty() && i - k + 1 > queue.peekFirst()) {
                 queue.removeFirst();
             }
+            //把queue里面小于当前值nums[i]的都pop掉，之后只需要拿后面的数(nums[j], j > i)和当前值nums[i]比较就可以了
+            //小于的数留着没用。如果queue里面还有数，就是这个数nums[i]之前的数(nums[k], k < i),虽然nums[k]现在比nums[i]大，
+            //但是随着windows的滑动，nums[k]很有可能就会被上面那个condition删除，nums[i]可以作为最大值的candidates
+            //这个while loop看上去好像会造成nk复杂度，但是因为其事所以element之后被add 或者remove一次，所以时间是O(n)
             while (!queue.isEmpty() && nums[i] > nums[queue.peekLast()]) {
                 queue.removeLast();
             }
