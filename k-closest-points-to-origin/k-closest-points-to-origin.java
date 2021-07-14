227c3df7e2e564377f7ec4261bde29239543c254
yangclement <54861086+yangclement@users.noreply.github.com>
2021-07-14 04:14:23 -0700

    Time: 28 ms (50.90%), Space: 47.4 MB (77.66%) - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.java
+++ k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -7,10 +7,12 @@ class Solution {
         if (points.length < k) {
             return points;
         }
+        //低级错误这里，算平方不能直接nums^2, 要么Math.pow(),但是这个input必须要是doulbe，最直接就nums * nums吧
         PriorityQueue<int[]> nearestKpoints = new PriorityQueue<int[]>((a, b) ->
                                                 b[0] * b[0] + b[1] * b[1] - a[0] * a[0] - a[1] * a[1]);
         for (int i = 0; i < points.length; i++) {
             nearestKpoints.offer(points[i]);
+            //保持heap里面只有k个elements
             if (nearestKpoints.size() > k) {
                 nearestKpoints.poll();
             }
