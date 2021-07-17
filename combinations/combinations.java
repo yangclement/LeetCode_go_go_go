af2e7b12573c7eb8a9ce6dccacb744c78d995267
yangclement <54861086+yangclement@users.noreply.github.com>
2021-07-16 17:24:22 -0700

    Time: 14 ms (78.69%), Space: 39.9 MB (97.24%) - LeetHub

---
+++ combinations/combinations.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+    //permutation是每个数都要选上，有顺序，所以每次recurrsion都要i = 0开始再看一遍(但跳过visited过的)
+    //combination无关顺序，前面已经看过的数字是不需要再看的，所以每次recurrsion都可以从跳过之前看过的index
+        List<List<Integer>> results = new ArrayList<>();
+        if (n == 0) {
+            return results;
+        }
+        LinkedList<Integer> curr = new LinkedList<Integer>();
+        findComb(results, curr, n, k, 1);
+        return results;
+    }
+    
+    private void findComb(List<List<Integer>> results, LinkedList<Integer> curr, int n, int k, int startIndex) {
+        
+        if (curr.size() == k) {
+            results.add(new ArrayList<Integer>(curr));
+            return;
+        }
+        
+        for (int i = startIndex; i <= n; i++) {
+            curr.addLast(i);
+            findComb(results, curr, n, k, i + 1);
+            curr.removeLast();
+        }
+    }
+}
