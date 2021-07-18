f917ca7652d4f43a4399beded22a2ba1b649c030
yangclement <54861086+yangclement@users.noreply.github.com>
2021-07-18 05:11:05 -0700

    Time: 26 ms (90.17%), Space: 55.6 MB (51.07%) - LeetHub

--- frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
+++ frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
@@ -1,5 +1,7 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) {
+        //同向双指针
+        //我感觉，一道双指针问题只有可能是同向，相向和背向，不可能参杂三个在一起
         //如果最大的利用k？
         //在一个array里面，如果我想看看k次操作能让几个数变成nums[j]
         //首先一定要sort这个array，j - 1这个位置是最容易变成j，用的最少的k，其次是 j - 2，因为这个题目只能increase不能decrease，所以不考虑j后面的数字
@@ -14,7 +16,7 @@ class Solution {
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
             //上一个loop的最大windows size sum + num[上一个数字]还没有检查过是否大于k，如果大于了就舍弃掉windows最左边的数，也就是离当前i最远的那个
-            //从上一个for loop过来的都是上一个能达到最大的window size < k
+            //从上一个for loop过来的都是上一个能达到最大的window size
             while (j < i && nums[i] * (i - j) - sum > k) {
                 sum -= nums[j++];
             }
