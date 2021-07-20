96cdcceac45032c374e8052e66081fe9119940dd
yangclement <54861086+yangclement@users.noreply.github.com>
2021-07-20 03:16:05 -0700

    Time: 18 ms (73.81%), Space: 42.4 MB (26.41%) - LeetHub

--- subarray-sum-equals-k/subarray-sum-equals-k.java
+++ subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -4,28 +4,55 @@ class Solution {
         if (nums == null || nums.length == 0) {
             return 0;
         }
+        
+        //解法三：基于解法一，解法一对于每个i都需要看i之前的每个sum[j]的value, 但是可以用hash map记录之前的sum[j]
+        //因为hash map的retrieve是O(1), 就可以优化到O(n)
+        //去hashmap找sum[i] - k这个key, value是 sum[i] - k出现的次数，就可以知道对于sum[i]为ending的有多少个等于k的subarray
+        int count = 0;
+        //key是sum，value是出现次数
+        Map<Integer, Integer> times = new HashMap<Integer, Integer>();
+        times.put(0, 1);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            //上个element i - 1的sum 加上当前array，然后去找sum - k
+            sum += nums[i];
+            int diff = sum - k;
+            count += times.getOrDefault(diff, 0);
+            //存下到目前i的sum，包括sum到map
+            times.put(sum, times.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+
+        
+        
+        
+        
+        
+        //解法二：
+        //从每个i开始，用sum记录i - end的结果 -> O(n ^ 2);
+        
+        
         //解法一：做一个到当前i的total sum，也就是sum (0 - i)，
         //当我想找subarray[j,i]是否合适是，只需要sum[i] - sum[j]
-        //runtime 是O(n);
-        int[] totalSum = new int[nums.length + 1];
-        totalSum[0] = 0;
-        int count = 0;
-        //先fill up这个totalSum array
-        for (int i = 1; i <= nums.length; i++) {
-            //当前位置i是i之前所有数的和，不包括i
-            totalSum[i] = totalSum[i - 1] + nums[i - 1];
-        }
-        for (int i = 0; i <= nums.length; i++) {
-                for (int j = 0; j < i; j++) {
-                    int subSum = totalSum[i] - totalSum[j];
-                    if (subSum == k) {
-                        count++;
-                    }
-                }
+        //runtime 是O(n^2);
+//         int[] totalSum = new int[nums.length + 1];
+//         totalSum[0] = 0;
+//         int count = 0;
+//         //先fill up这个totalSum array
+//         for (int i = 1; i <= nums.length; i++) {
+//             //当前位置i是i之前所有数的和，不包括i
+//             totalSum[i] = totalSum[i - 1] + nums[i - 1];
+//         }
+//         for (int i = 0; i <= nums.length; i++) {
+//                 for (int j = 0; j < i; j++) {
+//                     int subSum = totalSum[i] - totalSum[j];
+//                     if (subSum == k) {
+//                         count++;
+//                     }
+//                 }
 
-            }
-        return count;
-        
+//             }
+//         return count;
         
         //不可以使用two pointer也就是sliding windows
         //因为这个题目会contains负数
