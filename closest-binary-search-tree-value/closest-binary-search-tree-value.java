cc02ec58d4b528720289dab17257a48ae39c991a
yangclement <54861086+yangclement@users.noreply.github.com>
2021-07-21 04:37:07 -0700

    Time: 0 ms (100.00%), Space: 39.2 MB (20.61%) - LeetHub

--- closest-binary-search-tree-value/closest-binary-search-tree-value.java
+++ closest-binary-search-tree-value/closest-binary-search-tree-value.java
@@ -16,6 +16,8 @@
 class Solution {
     public int closestValue(TreeNode root, double target) {
         //使用while loop走 Node1 < target < Node2那一段即可
+        //runtime是O(h);
+        //solution的解法2，用stack储存之前的node，然后找一个node使得prev <= target < curr，然后根据diff return prev 还是curr
         int left = (int) target;
         int right = (int) Math.ceil(target);
         int result = 0;
