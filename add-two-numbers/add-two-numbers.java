9b4cb44b6b35b982ebb5be7dde04a567241a234c
yangclement <54861086+yangclement@users.noreply.github.com>
2021-08-11 10:24:33 -0700

    Time: 2 ms (59.38%), Space: 39.2 MB (63.74%) - LeetHub

--- add-two-numbers/add-two-numbers.java
+++ add-two-numbers/add-two-numbers.java
@@ -10,37 +10,84 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        boolean carry = false;
         ListNode dummy = new ListNode(0);
-        ListNode currNode = dummy;
+        ListNode curr = dummy;
+        boolean carry = false;
         while (l1 != null || l2 != null) {
-            int x = (l1 != null) ? l1.val : 0;
-            int y = (l2 != null) ? l2.val : 0;
-            int curr = x + y;
-            
+            int num1 = l1 == null? 0 : l1.val;
+            int num2 = l2 == null? 0 : l2.val;
+            int sum = num1 + num2;
             if (carry) {
-                curr++;
+                sum++;
                 carry = false;
             }
-            
-            if (curr >= 10) {
-                curr %= 10;
+            if (sum >= 10) {
                 carry = true;
+                sum %= 10;
             }
-            currNode.next = new ListNode(curr);
-            currNode = currNode.next;
+            curr.next = new ListNode(sum);
             if (l1 != null) {
                 l1 = l1.next;
             }
             if (l2 != null) {
                 l2 = l2.next;
             }
+            curr = curr.next;
         }
         if (carry) {
-            currNode.next = new ListNode(1);
+            curr.next = new ListNode(1);
         }
-        
         return dummy.next;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         boolean carry = false;
+//         ListNode dummy = new ListNode(0);
+//         ListNode currNode = dummy;
+//         while (l1 != null || l2 != null) { 
+//             int x = (l1 != null) ? l1.val : 0;
+//             int y = (l2 != null) ? l2.val : 0;
+//             int curr = x + y;
+            
+//             if (carry) {
+//                 curr++;
+//                 carry = false;
+//             }
+            
+//             if (curr >= 10) {
+//                 curr %= 10;
+//                 carry = true;
+//             }
+//             currNode.next = new ListNode(curr);
+//             currNode = currNode.next;
+//             if (l1 != null) {
+//                 l1 = l1.next;
+//             }
+//             if (l2 != null) {
+//                 l2 = l2.next;
+//             }
+//         }
+//         if (carry) {
+//             currNode.next = new ListNode(1);
+//         }
+        
+//         return dummy.next;
 
     }
 }
