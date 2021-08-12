8ebf55ae158a904406547ac9dd1aa5cafebe486f
yangclement <54861086+yangclement@users.noreply.github.com>
2021-08-11 20:57:15 -0700

    Time: 0 ms (100.00%), Space: 42.6 MB (17.68%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -6,30 +6,65 @@ class Solution {
         if (nums == null || nums.length  < 1) {
             return new int[] {-1, -1};
         }
+        int first = 0;
+        int last = 0;
         int start = 0;
         int end = nums.length - 1;
         while (start + 1 < end) {
             int mid = start + (end - start) / 2;
-            //就算找到了target也要继续往左走，因为当前有可能不是第一个position的element
-            if (nums[mid] == target) {
+            if (nums[mid] >= target) {
                 end = mid;
-            } else if (nums[mid] < target) {
+            } else {
+                start = mid;
+            }
+        }
+        if (nums[start] == target) {
+            first = start;
+        } else if (nums[end] == target){
+            first = end;
+        } else {
+            return new int[] {-1, -1};
+        }
+        start = 0;
+        end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] <= target) {
                 start = mid;
             } else {
                 end = mid;
             }
         }
-        //出来以后有start和end两个数还没有check.
-        int first = -1;
-        if (nums[start] == target) {
-            first = start;
-        } else if (nums[end] == target) {
-            first = end;
+        if (nums[end] == target) {
+            last = end;
+        } else {
+            last = start;
         }
-        int last = first;
-        while (first != -1 && last < nums.length - 1 && nums[last] == nums[last + 1]) {
-            last++;
-        }
-        return new int[]{first, last};
+        return new int[] {first, last};
+        // int start = 0;
+        // int end = nums.length - 1;
+        // while (start + 1 < end) {
+        //     int mid = start + (end - start) / 2;
+        //     //就算找到了target也要继续往左走，因为当前有可能不是第一个position的element
+        //     if (nums[mid] == target) {
+        //         end = mid;
+        //     } else if (nums[mid] < target) {
+        //         start = mid;
+        //     } else {
+        //         end = mid;
+        //     }
+        // }
+        // //出来以后有start和end两个数还没有check.
+        // int first = -1;
+        // if (nums[start] == target) {
+        //     first = start;
+        // } else if (nums[end] == target) {
+        //     first = end;
+        // }
+        // int last = first;
+        // while (first != -1 && last < nums.length - 1 && nums[last] == nums[last + 1]) {
+        //     last++;
+        // }
+        // return new int[]{first, last};
     }
 }
