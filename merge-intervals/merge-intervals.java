87788a81a400a332b8dd9b796c4c40d6049c0e3f
yangclement <54861086+yangclement@users.noreply.github.com>
2021-08-12 07:32:35 -0700

    Time: 5 ms (95.03%), Space: 41.9 MB (25.01%) - LeetHub

--- merge-intervals/merge-intervals.java
+++ merge-intervals/merge-intervals.java
@@ -1,51 +1,80 @@
 class Solution {
-    //starting: validation of the input: 1. negative number? 2.the intervals are always increasing?
-    //is start always greater than end?
     public int[][] merge(int[][] intervals) {
-        if(intervals.length==0 || intervals.length==1){
+        if (intervals == null || intervals.length <= 0) {
             return intervals;
         }
-        //sorted array fixed most of the problems
-        Arrays.sort(intervals, (a,b)->-b[0]+a[0]);
+        Arrays.sort(intervals, (a , b) -> a[0] - b[0]);
+        int lastStart = intervals[0][0];
+        int lastEnd = intervals[0][1];
         int[][] results = new int[0][2];
-        // boolean combined = false;
-        int[] combinedArray = intervals[0];
-        for (int i=1; i<intervals.length;i++){
-            int[] curr = combinedArray;
-            int[] next = intervals[i];
-            //condition 1: 可以combine
-            if(curr[1]-next[0]>=0){
-                combinedArray = new int[]{curr[0],Math.max(next[1],curr[1])};
-                // results = addElement(results, new int[]{curr[0],next[1]});
-                // combined = true;
-                if(i==intervals.length-1){
-                    results = addElement(results, combinedArray);
-                }
+        int i = 0;
+        for (int[] interval : intervals) {
+            int currStart = interval[0];
+            int currEnd = interval[1];
+            if (currStart <= lastEnd) {
+                lastEnd = Math.max(lastEnd, currEnd);
+            } else {
+                results = Arrays.copyOf(results, results.length + 1);
+                results[results.length - 1] = new int[] {lastStart, lastEnd};
+                lastStart = currStart;
+                lastEnd = currEnd;
             }
-            else {
-                results = addElement(results, combinedArray);
-                combinedArray = next;
-                if (i==intervals.length-1){
-                    results = addElement(results, next);
-                }
-                // combined = false;
-            }
-            // else{
-            //     results = addElement(results, curr);
-                //if the last two intervals are not overlap, since the foor loop is ended at i=length-                  //2, which will miss the last element from the original array. In this case,
-                // we need to add the last element to the result if the j pointer reaches the end.
-                // if(j == intervals.length-1){
-                //     results = addElement(results, next);
-                // }
-            }
+        }
+        results = Arrays.copyOf(results, results.length + 1);
+        results[results.length - 1] = new int[] {lastStart, lastEnd};
         return results;
-        }
+    }
     
-        public int[][] addElement(int[][] results,int[] result){
-            results  = Arrays.copyOf(results, results.length + 1);
-            results[results.length - 1] = result;
-            return results;
-        }
+    
+    
+    
+    //starting: validation of the input: 1. negative number? 2.the intervals are always increasing?
+    //is start always greater than end?
+    // public int[][] merge(int[][] intervals) {
+        // if(intervals.length==0 || intervals.length==1){
+        //     return intervals;
+        // }
+        // //sorted array fixed most of the problems
+        // Arrays.sort(intervals, (a,b)->-b[0]+a[0]);
+        // int[][] results = new int[0][2];
+        // // boolean combined = false;
+        // int[] combinedArray = intervals[0];
+        // for (int i=1; i<intervals.length;i++){
+        //     int[] curr = combinedArray;
+        //     int[] next = intervals[i];
+        //     //condition 1: 可以combine
+        //     if(curr[1]-next[0]>=0){
+        //         combinedArray = new int[]{curr[0],Math.max(next[1],curr[1])};
+        //         // results = addElement(results, new int[]{curr[0],next[1]});
+        //         // combined = true;
+        //         if(i==intervals.length-1){
+        //             results = addElement(results, combinedArray);
+        //         }
+        //     }
+        //     else {
+        //         results = addElement(results, combinedArray);
+        //         combinedArray = next;
+        //         if (i==intervals.length-1){
+        //             results = addElement(results, next);
+        //         }
+        //         // combined = false;
+        //     }
+        //     // else{
+        //     //     results = addElement(results, curr);
+        //         //if the last two intervals are not overlap, since the foor loop is ended at i=length-                  //2, which will miss the last element from the original array. In this case,
+        //         // we need to add the last element to the result if the j pointer reaches the end.
+        //         // if(j == intervals.length-1){
+        //         //     results = addElement(results, next);
+        //         // }
+        //     }
+        // return results;
+//         }
+    
+//         public int[][] addElement(int[][] results,int[] result){
+//             results  = Arrays.copyOf(results, results.length + 1);
+//             results[results.length - 1] = result;
+//             return results;
+//         }
         
         
     }
