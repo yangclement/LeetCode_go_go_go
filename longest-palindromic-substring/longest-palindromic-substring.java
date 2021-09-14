46714bf5cd9010af4e4d2935756e89b1f7ccfb00
yangclement <54861086+yangclement@users.noreply.github.com>
2021-09-13 18:07:19 -0700

    Time: 56 ms (45.62%), Space: 39.7 MB (50.36%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.java
+++ longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,39 +1,32 @@
 class Solution {
-    //input constrain? empty space inside? empty string?
     public String longestPalindrome(String s) {
-        if(s.length()==0||s==null){
+        if (s.length() == 0 || s == null) {
             return "";
         }
-        char[] charArray = s.toCharArray();
         int start = 0;
         int end = 0;
-        for(int i=0;i<s.length();i++){
-            //for odd longest substring
-            int len1 = findMaxLength(i,i,charArray);
-            //dont need this condition, because the while loop in the helper function 
-            //will help to check
-            // if(i!==s.length-1 & charArray[i]==charArray[i+1]){
-            //     len2 = findMaxLength(i,i+1,charArray);
-            // }
-            //for even longest substring
-            int len2 = findMaxLength(i,i+1,charArray);
-            //use the len1 and len2 to compare to the (end - start), if so, update start and end
-            int currLength = Math.max(len1,len2);
-            if(currLength> end-start){
-                start = i - ((currLength-1)/2);
-                end = i + currLength/2;
+        for (int i = 0; i < s.length(); i++) {
+            int oddLength = findLength(i, i, s);
+            int evenLength = 0;
+            if (i != 0) {
+                evenLength = findLength(i - 1, i, s);
+            }
+            
+            int currLength = Math.max(oddLength, evenLength);
+            if (currLength > end - start) {
+                end = i + (currLength / 2);
+                start = i - ((currLength - 1) / 2);
             }
         }
-        return s.substring(start,end+1);
+        //aa a aa
+        return s.substring(start, end);
+    }
     
-}
-    private int findMaxLength(int center1,int center2, char[] charArray){
-        int left = center1;
-        int right = center2;
-        while(left>=0 && right<charArray.length && charArray[left]==charArray[right]){
+    private int findLength(int left, int right, String s) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
         }
-        return right-left-1;
+        return right - left;
     }
 }
