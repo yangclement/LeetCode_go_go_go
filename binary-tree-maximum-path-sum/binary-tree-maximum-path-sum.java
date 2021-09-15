a3a20896f1a5416eb3237d965cb13173e7b88000
yangclement <54861086+yangclement@users.noreply.github.com>
2021-09-15 02:46:39 -0700

    Time: 0 ms (100.00%), Space: 40.8 MB (67.04%) - LeetHub

--- binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -14,34 +14,22 @@
  * }
  */
 class Solution {
-    public int max;
+    public int max = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        //最开始思路：recusively traversal 这个tree，看看联通左右tree的话，是否能够更大
-        //不能的话，把当前node加上左边或者加上右边，看看哪边大，然后返回给上一层
-        //pass了，但是用了global variable；
-        //如果不用global可以把max 和 当前路径的sum放进一个class里面return，就像是return两个values同理
-        //runtime是 O(n), space O(H), H是tree的height
-        if (root == null) {
-            return 0;
-        }
-        max = Integer.MIN_VALUE;
-        findSumRecurrsion(root);
+        findPath(root);
         return max;
     }
     
-    private int findSumRecurrsion(TreeNode curr) {
+    private int findPath(TreeNode curr) {
         if (curr == null) {
             return 0;
         }
-        //一共有几总可能性对于当前的node？
-        //左边加当前加上右边，当前自己，左边加自己，右边加自己，只有后三种需要return上去
-        int left = findSumRecurrsion(curr.left);
-        int right = findSumRecurrsion(curr.right);
-        int result = Math.max(left + curr.val, right + curr.val);
-        result = Math.max(result, curr.val);
-        max = Math.max(max, result);
-        max = Math.max(max, curr.val + left + right);
-        return result;
-        
+        int left = findPath(curr.left);
+        int right = findPath(curr.right);
+        int results = Math.max(curr.val, Math.max(left + curr.val, right + curr.val));
+        max = Math.max(max, results);
+        max = Math.max(max, left + right + curr.val);
+
+        return results;
     }
 }
