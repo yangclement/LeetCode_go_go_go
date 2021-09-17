21109fe6f538f9900b4aa535e5f6f0bde8251bb3
yangclement <54861086+yangclement@users.noreply.github.com>
2021-09-16 23:18:34 -0700

    Time: 1 ms (98.32%), Space: 41.7 MB (21.16%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,29 +1,26 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        //quickselect, similar to quicksort
-        // if (nums == null) {
-        //     return null;
-        // }
-        int result = quickselect(0, nums.length - 1, k, nums);
-        return result;
+        if (nums == null) {
+            return -1;
+        }
+        int results = quickSelect(nums, k, 0, nums.length - 1);
+        return results;
     }
     
-    private int quickselect(int start, int end, int k, int[] nums) {
+    private int quickSelect(int[] nums, int k, int start, int end) {
         if (start >= end) {
             return nums[start];
         }
-        
         int left = start;
         int right = end;
-        int pivot = nums[start + (end - start) / 2];
+        int mid = nums[start + ((end - start) / 2)];
         while (left <= right) {
-            while (left <= right && nums[left] > pivot) {
+            while (left <= right && nums[left] > mid) {
                 left++;
             }
-            while (left <= right && nums[right] < pivot) {
+            while (left <= right && nums[right] < mid) {
                 right--;
             }
-            // if left == right, will enter this condition, which could cause there is a number between left and right. We need to dead with this number later
             if (left <= right) {
                 int temp = nums[left];
                 nums[left] = nums[right];
@@ -32,25 +29,12 @@ class Solution {
                 right--;
             }
         }
-            
-            //The above part is the same as the quicksort.
-            //here, the array is possibly divided into three part.
-            //left, middle (from the condition above), right.
-            //k in left side.
-            if (start + k - 1 <= right) {
-                return quickselect(start, right, k, nums);
-            }
-            //k in the right side.
-            if (start + k - 1 >= left) {
-                //here should not be k - left for the new k
-                //这个时候要走右边，左边会有 left - start 个数，对于k来说就少了 left -start个数，
-                //所以新的k = k - (left - start)
-                return quickselect(left, end, k - (left - start), nums);
-            }
-            
-            // If the code reaches here, it means there is a middle part
-            return nums[right + 1];
-            
-        
+        if (start + k - 1 >= left) {
+            return quickSelect(nums, k - (left - start), left, end);
+        }
+        if (start + k - 1 <= right) {
+            return quickSelect(nums, k, start, right);
+        }
+        return nums[right + 1];
     }
 }
